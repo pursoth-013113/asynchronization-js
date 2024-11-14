84c3af9db71a7da59a0f035914015c421c7d5e26
pursoth-013113 <122788660+pursoth-013113@users.noreply.github.com>
2024-11-14 15:12:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+
+    // setTimeout
+    // setTimeout(()=>{
+    //     console.log('hello')
+    // },2000)
+    
+    // // setInterval
+    // setInterval(() => {
+    //     console.log('bye')
+    // }, 4000);
+
+    //  synchronization
+    //   it will wait for oneby line
+
+    // asynchronization 
+    // it will not wait for oneby line
+
+    // let food
+
+    // function order (et)
+    // {
+    //     setTimeout(()=>{
+    //         food='pizza'
+    //         et()
+    //     },2000)
+    // }
+    // function eat()
+    // {
+    //     console.log(food)
+
+    // }
+    // order(eat)
+
+    // function demo()
+    // {
+    //     return new Promise ((resolve,reject)=>{
+    //         resolve('hello')
+    //         // or reject
+    //     })
+    // }
+
+    // assingcronass type
+// Promises
+//    1-bending
+//    2-resolve
+//    3-reject
+
+
+// let url='https://superheroapi.com/api.php/6fff236defeac5fff485e315d811247f/100'
+
+// fetch(url)
+// .then(res=>res.json())
+// .then(data=>document.getElementById('section').innerHTML=`<img src=${data.image.url} height=900px width=600px>`)
+// let url = 'https://superheroapi.com/api.php/6fff236defeac5fff485e315d811247f/30';  
+//       function display() {
+//          let id = Math.ceil(Math.random() * 730);
+
+//          fetch(`${url}/${id}`)  
+//             .then(res => res.json())
+//             .then(data =>
+//                document.getElementById('section').innerHTML = `<img src=${data.image.url} height=250px width=300px>`
+//             )  
+//       }
+
